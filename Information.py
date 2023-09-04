8d40a36b5a0a3c10e8d061bc711382501996ddef
punn <punn123455@gmail.com>
2023-09-04 13:16:07 +0700

    now return information

---
+++ Information.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+import os
+from ultralytics.yolo.engine.results import Results
+from ultralytics.yolo.engine.predictor import BasePredictor
+
+def get_yolo_result(img=None):
+    if img is None:
+        return {}
+
+    model = YOLO("YoloModel//silk2_EN.pt")
+    predict = model.predict(source=img, save=True, show=False, project=os.getcwd())
+
+      #count each
+    result_names = predict[0].names
+    classic = predict[0].boxes.cls
+    classic = [int(x.item()) for x in classic]
+    count = {}
+    for name in classic:
+        count[result_names[name]] = count.get(result_names[name], 0) + 1
+    #print(count)
+    
+    #accu = predict[0].boxes.conf
+    #print(accu)
+
+      #position
+    posi = predict[0].boxes.xyxy
+    posi = [t.numpy() for t in posi]
+    #print(posi)
+    
+    #class_ids_list =  list(predict[0].boxes.cls)
+
+      #savedir
+    leen = predict[0].save_dir
+    a ="C:\\Users\\USER\\Desktop\\yoloML\\ultralytics\\test\\IMG_8115.JPG"
+    b = os.path.basename(a)
+    path_direc = os.path.join(leen,b)
+    #print(path_direc)
+    
+    return count , posi , path_direc
+
+get_yolo_result("C:\\Users\\USER\\Desktop\\yoloML\\ultralytics\\test\\IMG_8115.JPG") 
